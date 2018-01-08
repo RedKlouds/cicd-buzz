f44a8a32ea53245bef9ea8bf501d70c915ab7181
Danny ly <dannyly199@gmail.com>
2018-01-08 01:08:21 -0800

    edited synatax error

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -10,11 +10,11 @@ def test_sample_single_word():
 
 def test_sample_multiple_words():
 	l = ('food','bar','foodbar')
-	words = generator.sample(1,2)
+	words = generator.sample(l,2)
 	assert len(words) == 2
 	assert words[0] in l
 	assert words[1] in l
-	assert words[0] is not words[l]
+	assert words[0] is not words[1]
 def test_generate_buzz_of_at_least_five_words():
 	pharse = generator.generate_buzz()
 	assert len(pharse.split()) >= 5
