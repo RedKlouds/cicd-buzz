7b6ab5961020d9bfa507657d382c4312ad54d299
Danny ly <dannyly199@gmail.com>
2018-01-08 02:21:41 -0800

    changed docker deploy config

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker login -u="redklouds" -p="$HondaperHH1"
+docker login -u=$DOCKER_USER -p=$DOCKER_PASS
 
-if ["$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
 	TAG="latest"
 else
 	TAG="$TRAVIS_BRANCH"
 fi
 
-docker build -f Dockerfile -t "redklouds/cicd-buzz":$TAG .
+docker build -f Dockerfile -t redklouds/cicd-buzz:$TAG .
 docker push redklouds/cicd-buzz:$TAG
